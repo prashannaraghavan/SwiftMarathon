cbe00a7a5bcf57db36dd22c8fe4a114765bfbb6f
Prashanna Raghavan <prashanna.raghavan@gmail.com>
2017-07-04 21:36:25 -0700

    SortSearch

--- FewMore.playground/Contents.swift
+++ FewMore.playground/Contents.swift
@@ -125,6 +125,92 @@ class Insertion
 }
 
 var insertion = Insertion()
-var array = [4,3,2,7,5,8,1,9,6,0]
+var array = [9,8,7,6,5,4,3,2,1,0]
 insertion.sort(num: &array)
+
+class Bubble
+{
+    func swap(a:inout Int,b:inout Int)
+    {
+        let temp = a
+        a = b
+        b = temp
+    }
+    
+    func sort(num :inout [Int]) -> [Int] {
+        for i in 0..<num.count {
+            for j in i+1..<num.count
+            {
+                if num[j] < num[i] { swap(a: &num[i], b: &num[j]) }
+            }
+        }
+        
+        return num
+    }
+}
+
+var bubble = Bubble()
+var bubblearray = [4,3,2,7,5,8,1,9,6,0]
+bubble.sort(num: &bubblearray)
+print(bubblearray.sorted(by: {$0 < $1}))
+
+class Fibonacci
+{
+    var fib = [Int]()
+    func numbers(num :Int) -> [Int] {
+        for i in 0...num
+        {
+            if i==0 || i==1 {
+                fib.insert(i, at: i)
+            }
+                
+            else{
+                fib.insert(fib[i-1] + fib[i-2], at: i)
+            }
+        }
+        return fib
+    }
+}
+
+var fibo = Fibonacci()
+fibo.numbers(num: 10)
+
+
+extension Int
+{
+    func repetition(task: ()->Void)  {
+        for _ in 0..<self{
+            task()
+        }
+    }
+}
+
+3.repetition {
+    print("Hello")
+}
+
+class BinarySearch
+{
+    func bin(_ num:Int,_ start:Int,_ end:Int,_ inarr:[Int]) -> Int {
+        let mid = (start+end)/2
+        if num == inarr[mid] {
+            return mid
+        }
+        else if num < inarr[mid] {
+            return bin(num,0,mid,inarr)
+        }
+        else{
+            return bin(num,mid+1,end,inarr)
+        }
+    }
+    
+    func find(num:Int,inarr:[Int]) -> Int {
+        let sortedarr = inarr.sorted(by: {$0 < $1})
+        return bin(num,0,sortedarr.count,sortedarr)
+    }
+}
+
+var bin = BinarySearch()
+let ar = [4,5,3,2,1]
+bin.find(num: 5,inarr:ar)
 
